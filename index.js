329e423a6f2ba2121597056250919e7cc2eeac63
er84yaabhishek <er84yaabhishek@gmail.com>
2023-03-04 16:08:00 +0530

    this is pure nodejs with connection of mongo db:

---
+++ index.js
@@ -0,0 +1,12 @@
+const{MongoClient}=require("mongodb");
+const url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url);
+const database = 'e-com'
+async function getData(){
+    let result = await client.connect();
+    let db = result.db(database);
+    let collection = db.collection('products');
+    let response = await collection.find({}).toArray();
+    console.log(response);
+}
+getData();
